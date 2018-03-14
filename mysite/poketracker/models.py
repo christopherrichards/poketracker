5924c0dbc382a4bfb7fdaf013d42f012a626299d
Christopher Richards <mr.crichards@gmail.com>
2018-03-14 14:27:38 -0700

    Removed committed db, Created Pokemon model (contains name, which is output in __str__)

--- mysite/poketracker/models.py
+++ mysite/poketracker/models.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Pokemon(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
