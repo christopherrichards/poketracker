fc9039b346fc11abb37eb4b28651d9f745619d6d
Christopher Richards <mr.crichards@gmail.com>
2018-03-15 00:33:31 -0700

    Created evolve-new view

--- mysite/poketracker/views.py
+++ mysite/poketracker/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.db.models import F
 
 from .models import Pokemon
 
@@ -22,6 +23,13 @@ class UncaughtView(generic.ListView):
 
     def get_queryset(self):
         return Pokemon.objects.all().filter(caught=False)
+
+
+class EvolveNewView(generic.ListView):
+    template_name = 'poketracker/index.html'
+
+    def get_queryset(self):
+        return Pokemon.objects.all().filter(caught=False, numCandies__gt=F('candiesToEvolve'))
 
 
 class DetailView(generic.DetailView):
